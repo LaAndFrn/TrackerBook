1f94a6474d41766c056a9aaf8e22e0b45d75adc9
Laura <maiden_street@hotmail.com>
2024-08-25 12:16:37 -0500

    tercera parte de archivos

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ document.getElementById('cifrarButton').addEventListener('click', function() {
     let textoEntrada = document.getElementById('areaTextoInput').value;
     let textoResultado = cifrarTexto(textoEntrada); 
     document.getElementById('resultadoTexto').innerText = textoResultado;
+    document.getElementById('copiarButton').style.display = 'block';
 });
 
 document.getElementById('descifrarButton').addEventListener('click', function() {
     let textoEntrada = document.getElementById('areaTextoInput').value;
     let textoResultado = descifrarTexto(textoEntrada); 
     document.getElementById('resultadoTexto').innerText = textoResultado;
+    document.getElementById('copiarButton').style.display = 'block';
 });
 
 
